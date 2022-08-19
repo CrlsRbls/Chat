5b96577ec2f1c5cb88d1e35fdfdaa04cb31539f4
CrlsRbls <ct.r0622gmail.com>
2022-08-18 21:07:36 -0400

    added avatars

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,17 +4,22 @@ class Room < ApplicationRecord
    after_create_commit { broadcast_if_public }
    has_many :messages
    has_many :participants, dependent: :destroy
-
+ 
    def broadcast_if_public
-     broadcast_if_public "rooms" unless self.is_private
+     broadcast_append_to 'rooms' unless is_private
+   end
+ 
+   def self.create_private_room(users, room_name)
+     single_room = Room.create(name: room_name, is_private: true)
+     users.each do |user|
+       Participant.create(user_id: user.id, room_id: single_room.id)
+     end
+     single_room
    end
 
-   def self.create_private_room(users, room_name)
-      single_room = Room.create(name: room_name, is_private: true)
-      users.each do |user|
-         Participant.create(user_id: user.id, room_id: single_room.id)
-      end
-      single_room
+   def participant?(room, user)
+    room.participants.where(user: user).exists?
+    Participant.where(user_id: user.id, room_id: room.id).exists?
    end
 end
  
